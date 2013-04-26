99899a2d2fa9b9bacf94fe85535f424986301fad
Angelo Dini <finalangeljp@hotmail.com>
2013-04-26 15:39:05 +0200

    cleanup

--- src/manage.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from django.core.management import ManagementUtility
-
-if __name__ == "__main__":
-    utility = ManagementUtility(None)
-    utility.execute()
--- src/settings_server_stage.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-from settings_server import *

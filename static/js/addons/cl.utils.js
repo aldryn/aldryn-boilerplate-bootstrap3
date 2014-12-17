e6803826db1840e1b90fdd5eb2857525644c1432
FinalAngel <angelo.dini@divio.ch>
2014-12-17 11:31:13 +0100

    wrap everything in an IIFE

--- static/js/addons/cl.utils.js
+++ static/js/addons/cl.utils.js
@@ -1,24 +1,33 @@
-'use strict';
+/*!
+ * @author:    Divio AG
+ * @copyright: http://www.divio.ch
+ */
+
 //######################################################################################################################
 // #NAMESPACES#
 var Cl = window.Cl || {};
 
 //######################################################################################################################
-/* global Cl */
-Cl.Utils = {
+// #UTILS#
+(function ($) {
+    'use strict';
 
-    // INFO: general document setup for no javascript fallbacks and logging
-    document: function () {
-        // remove no-js class if javascript is activated
-        $(document.body).removeClass('noscript');
-        // ensure console.log doesn't throw errors
-        if (!window.console) { window.console = { 'log': function () {} }; }
-    },
+    Cl.Utils = {
 
-    // INFO: simple mobile and tablet filter that can be extended
-    mobile: function () { return (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/Android/i)); },
-    tablet: function () { return (navigator.userAgent.match(/iPad/i)); }
+        // INFO: general document setup for no javascript fallbacks and logging
+        document: function () {
+            // remove no-js class if javascript is activated
+            $(document.body).removeClass('noscript');
+            // ensure console.log doesn't throw errors
+            if (!window.console) { window.console = { 'log': function () {} }; }
+        },
 
-    // INFO: add here more Utilities required for the project
+        // INFO: simple mobile and tablet filter that can be extended
+        mobile: function () { return (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/Android/i)); },
+        tablet: function () { return (navigator.userAgent.match(/iPad/i)); },
 
-};
+        // INFO: add here more Utilities required for the project
+
+    };
+
+})(jQuery);

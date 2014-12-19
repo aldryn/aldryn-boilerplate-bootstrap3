62415494dbf38ca7f42dfd48ee8cd89a269a8e1a
FinalAngel <angelo.dini@divio.ch>
2014-12-19 08:56:02 +0100

    re add configuration

--- docs/conf.py
+++ docs/conf.py
@@ -166,7 +166,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'aldryn-docsdoc'
+htmlhelp_basename = 'boilerplate'
 
 
 # -- Options for LaTeX output --------------------------------------------------

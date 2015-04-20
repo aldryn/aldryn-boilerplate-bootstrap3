2dea05daabdfbea273e9d773434c508cdd098008
FinalAngel <angelo.dini@divio.ch>
2015-04-20 16:34:59 +0200

    added more tests

--- tests/test.cl.utils.js
+++ tests/test.cl.utils.js
@@ -28,6 +28,47 @@
                 expect(Cl.Utils._consoleWrapper).toHaveBeenCalled();
             });
         });
+
+        describe('redirectTo()', function () {
+            it('redirects you to a new url', function () {
+                expect(window.location.href).not.toMatch('#testRedirect');
+                Cl.Utils.redirectTo('#testRedirect');
+                expect(window.location.href).toMatch('#testRedirect');
+            });
+        });
+
+        describe('setStorage()', function () {
+            it('checks if localstorage is available', function () {
+                if (typeof (Storage) !== void(0)) {
+                    expect(Storage).toEqual(jasmine.any(Object));
+                } else {
+                    expect(Storage).toThrowError(TypeError);
+                }
+            });
+            it('passes a token and value to localstorage', function () {
+                localStorage.clear();
+                expect(localStorage.getItem('test#1')).toBeNull();
+                Cl.Utils.setStorage('test#1', 'true');
+                expect(localStorage.getItem('test#1')).toBe('true');
+            });
+        });
+
+        describe('getStorage()', function () {
+            it('checks if localstorage is available', function () {
+                if (typeof (Storage) !== void(0)) {
+                    expect(Storage).toEqual(jasmine.any(Object));
+                } else {
+                    expect(Storage).toThrowError(TypeError);
+                }
+            });
+            it('retrieves the value of a token', function () {
+                localStorage.clear();
+                localStorage.setItem('test#2', 'true');
+                expect(localStorage.getItem('test')).toBeNull();
+                var storage = Cl.Utils.getStorage('test#2');
+                expect(storage).toBe('true');
+            });
+        });
     });
 
 })();

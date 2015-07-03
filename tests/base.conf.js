55c06d33492e481aa07d57fbe9df068861460d21
Angelo Dini <angelo.dini@divio.ch>
2015-07-03 14:23:53 +0200

    add new testing structure

---
+++ tests/base.conf.js
@@ -0,0 +1,25 @@
+module.exports = {
+    formatTaskName: function formatTaskName (browserName) {
+        return [
+            'Test', browserName, 'for',
+            process.env.TRAVIS_REPO_SLUG,
+            (process.env.TRAVIS_PULL_REQUEST ? 'pull request #' + process.env.TRAVIS_PULL_REQUEST : ''),
+            'build #' + process.env.TRAVIS_BUILD_NUMBER
+        ].join(' ');
+    },
+
+    sauceLabsBrowsers: {
+        sl_ie_9: {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            platform: 'Windows 7',
+            version: '9.0'
+        },
+        sl_firefox: {
+            base: 'SauceLabs',
+            browserName: 'firefox',
+            platform: 'Windows 8',
+            version: '38'
+        }
+    }
+};

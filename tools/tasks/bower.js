79b53796210aeebfbfab098a674e90127acae394
Vadim Sikora <vadim.sikora@divio.ch>
2016-01-27 20:12:56 +0100

    unrecurse gulp tasks

--- tools/tasks/bower.js
+++ tools/tasks/bower.js
@@ -4,8 +4,6 @@ var bower = require('gulp-bower');
 
 module.exports = function (gulp, opts) {
     return function () {
-        gulp.task('bower', function () {
-            return bower(gulp.dest(opts.PROJECT_ROOT + opts.PROJECT_PATH.bower));
-        });
+        return bower(gulp.dest(opts.PROJECT_PATH.bower));
     };
 };
--- tools/tasks/sass.js
+++ tools/tasks/sass.js
@@ -9,31 +9,29 @@ var gulpif = require('gulp-if');
 
 module.exports = function (gulp, opts) {
     return function () {
-        gulp.task('sass', function () {
-            gulp.src(opts.PROJECT_PATTERNS.sass)
-                // sourcemaps can be activated through `gulp sass --debug´
-                .pipe(gulpif(opts.DEBUG, sourcemaps.init()))
-                .pipe(sass())
-                .on('error', function (error) {
-                    gutil.log(gutil.colors.red(
-                        'Error (' + error.plugin + '): ' + error.messageFormatted)
-                    );
-                    // used on Aldryn to inform aldryn client about the errors in
-                    // SASS compilation
-                    if (process.env.EXIT_ON_ERRORS) {
-                        process.exit(1);
-                    }
-                })
-                .pipe(autoprefixer({
-                    // browsers are coming from browserslist file
-                    cascade: false
-                }))
-                .pipe(minifyCss({
-                    rebase: false
-                }))
-                // sourcemaps can be activated through `gulp sass --debug´
-                .pipe(gulpif(opts.DEBUG, sourcemaps.write()))
-                .pipe(gulp.dest(opts.PROJECT_PATH.css));
-        });
+        gulp.src(opts.PROJECT_PATTERNS.sass)
+            // sourcemaps can be activated through `gulp sass --debug´
+            .pipe(gulpif(opts.DEBUG, sourcemaps.init()))
+            .pipe(sass())
+            .on('error', function (error) {
+                gutil.log(gutil.colors.red(
+                    'Error (' + error.plugin + '): ' + error.messageFormatted)
+                );
+                // used on Aldryn to inform aldryn client about the errors in
+                // SASS compilation
+                if (process.env.EXIT_ON_ERRORS) {
+                    process.exit(1);
+                }
+            })
+            .pipe(autoprefixer({
+                // browsers are coming from browserslist file
+                cascade: false
+            }))
+            .pipe(minifyCss({
+                rebase: false
+            }))
+            // sourcemaps can be activated through `gulp sass --debug´
+            .pipe(gulpif(opts.DEBUG, sourcemaps.write()))
+            .pipe(gulp.dest(opts.PROJECT_PATH.css));
     };
 };
